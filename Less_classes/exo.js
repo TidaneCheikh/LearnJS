2c9992aabd7e0a7aadb215ea3d8fb1ceedd18a35
cheikhtidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2025-07-17 10:33:53 +0000

    c

--- Less_classes/exo.js
+++ Less_classes/exo.js
@@ -1,14 +1,5 @@
-function collatzSequence(n) {
-    let sequence = [];
-    while (n !== 1) {
-        sequence.push(n);
-        if (n % 2 === 0) {
-            n /= 2;
-        } else {
-            n = 3 * n + 1;
-        }
-    }
-    sequence.push(1); // Ajouter le dernier élément 1
-    console.log(sequence.join(" "));
-}
-
+let i=0;
+do{
+    console.log("valeur : ", i);
+    i++;
+}while(i<=10)
