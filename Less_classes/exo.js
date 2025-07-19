b46afef7d42a15e5dc86262ad74d28a9031c6934
cheikhtidiane <cheikhtidiane15.thiam@ucad.edu.sn>
2025-07-19 23:03:03 +0000

    filter

--- Less_classes/exo.js
+++ Less_classes/exo.js
@@ -1,5 +1,4 @@
 let nombre=[12 ,15]
-nombre =nombre.map(x=> x*2)
-for(let i=0 ;i<nombre.length;i++){
-    console.log(nombre[i])
-}
+
+carre = nombre.filter(n=>n==12).map(n=>n*2)
+console.log(carre);
